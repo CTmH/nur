8f066892db8055ff007cfd4e16b178571e768074
CTmH <han.zhang@ctmh.top>
2024-10-28 13:54:31 +0300

    remove example package
    Signed-off-by: CTmH <han.zhang@ctmh.top>

--- default.nix
+++ default.nix
@@ -14,7 +14,6 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
   super-productivity = pkgs.callPackage ./pkgs/super-productivity { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
